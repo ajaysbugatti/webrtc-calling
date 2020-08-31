5dc8b941a5040e1941111e31d11c5b18dddeb802
Ajay <ajay.dayma706@gmail.com>
2020-09-01 04:33:07 +0530

    start script

--- public/scriptAud.js
+++ public/scriptAud.js
@@ -2,7 +2,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-  host: 'localhost',
+  host: 'morning-fjord-33054.herokuapp.com',
   port: '9005',
   path: '/myapp'
 })
